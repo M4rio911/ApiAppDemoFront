3285738c3fe5013145a153f4ea18a9b528149197
Mariusz Środoń <msrodon@9bits.pl>
2025-06-27 17:57:46 +0200

    Add HTTP and BE alerts

--- ApiAppDemoFront/src/utils/api.js
+++ ApiAppDemoFront/src/utils/api.js
@@ -13,12 +13,11 @@ export default class ApiService {
           'Authorization': `Bearer ${this.token}`
         }
       };
-    
+
       if (data) {
         options.headers['Content-Type'] = 'application/json';
         options.body = JSON.stringify(data);
       }
-    
       try {
         const response = await fetch(this.baseUrl + endpoint, options);
         const contentType = response.headers.get('content-type');
@@ -32,9 +31,43 @@ export default class ApiService {
           status: response.status
         };
 
+        if (response.status >= 400) {
+          let message = 'An error occurred';
+
+          switch (response.status) {
+            case 400:
+              message = responseJson.message || 'Bad Request';
+              break;
+            case 401:
+              message = 'Unauthorized – please login again.';
+              break;
+            case 403:
+              message = 'Forbidden – you do not have access.';
+              break;
+            case 404:
+              console.log(response.status);
+              message = 'Not Found – the resource does not exist.';
+              break;
+            case 500:
+              message = 'Internal Server Error – try again later.';
+              break;
+          }
+
+          alert(message )
+        }
+
+        else if (responseJson.Errors && Array.isArray(responseJson.Errors)) {
+          responseJson.Errors.forEach(message => {
+            if (this.notify) {
+              alert(message);
+            }
+          });
+        }
+
         return responseJson;
       } catch (error) {
-          throw error;
+        alert('Network Error ' + error.message);
+        throw error;
       }
     }
   
